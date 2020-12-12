cfe0cd29e1e0404ff960edba270e90fe65034693
truonghuusang99 <52596748+truonghuusang99@users.noreply.github.com>
2020-12-12 23:28:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,91 +1,131 @@
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 3000;
-const urlDB = 'mongodb://localhost:27017/BANDO'
-const mongo = new MongoClient(urlDB)
-
-//init variable
-const floor = {
-    type: "FeatureCollection",
-    generator: "overpass-ide",
-    copyright: "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
-    timestamp: "2020-08-27T10:45:03Z",
-    features: []
-}
-const room = {
-    type: "FeatureCollection",
-    generator: "overpass-ide",
-    copyright: "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
-    timestamp: "2020-08-27T10:45:03Z",
-    features: []
-}
+const urlDB = "mongodb://localhost:27017/localhost";
+const mongo = new MongoClient(urlDB, { useUnifiedTopology: true });
 
 // FUNCTION QUERY
 
-async function queryDataFloor() {
+function queryDataFloor() {
+  return new Promise(async (resolve, rejects) => {
     try {
-        await mongo.connect()
-        console.log("Connected correctly to server");
-        const database = await mongo.db("block_b")
+      const floor = {
+        type: "FeatureCollection",
+        generator: "overpass-ide",
+        copyright:
+          "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+        timestamp: "2020-08-27T10:45:03Z",
+        features: [],
+      };
 
-        const _FLOOR_PROP = await database.collection("Body").find({ type: { $eq: "floor" } }).toArray()
-        _FLOOR_PROP.forEach(async floorProp => {
-            let feature = {
-                type: "Feature",
-                properties: floorProp,
-                geometry: {
-                    type: "Polygon",
-                    coordinates: []
-                }
-            }
-            let faceFloor = await database.collection("Face").findOne({ id_floor_room: floorProp._id.toString() })
-            // tìm tọa độ
-            let geometries = await (await database.collection("Node").find({ id_face: faceFloor._id.toString() }).sort(['index'], 1).toArray()).map(item => item.geometry)
-            feature['geometry']['coordinates'].push(geometries)
+      await mongo.connect();
+      console.log("Connected correctly to server");
+      const database = await mongo.db("block_b");
 
-            floor['features'].push(feature)
-        })
+      const _FLOOR_PROP = await database
+        .collection("BODY")
+        .find({ "graphic:type": { $eq: "floor" } })
+        .toArray();
+      for (let i = 0; i < _FLOOR_PROP.length; i++) {
+        _FLOOR_PROP[i]["_id"].toString();
+        let feature = {
+          type: "Feature",
+          properties: _FLOOR_PROP[i],
+          geometry: {
+            type: "Polygon",
+            coordinates: [],
+          },
+        };
+        let faceFloor = await database
+          .collection("FACE")
+          .findOne({ id_body: _FLOOR_PROP[i]._id.toString() });
+        // tìm tọa độ
+        let geometries = await (
+          await database
+            .collection("NODE")
+            .find({ id_face: faceFloor._id.toString() })
+            .sort(["index"], 1)
+            .toArray()
+        ).map((item) => item.geometry);
 
+        feature["geometry"]["coordinates"].push(geometries);
+        floor["features"].push(feature);
+      }
+      resolve(floor);
     } catch (err) {
-        throw err
-    } finally {
-        //await mongo.close(true)
+      resolve({ error: err });
+      throw err;
     }
+  });
 }
 
-async function queryDataRoom() {
+function queryDataRoom(res) {
+  return new Promise(async (resolve, rejects) => {
     try {
-        
-    } catch (error) {
-        
-    } finally {
+      const room = {
+        type: "FeatureCollection",
+        generator: "overpass-ide",
+        copyright:
+          "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+        timestamp: "2020-08-27T10:45:03Z",
+        features: [],
+      };
 
+      await mongo.connect();
+      console.log("Connected correctly to server");
+      const database = await mongo.db("block_b");
+
+      const _ROOM_PROP = await database
+        .collection("BODY")
+        .find({ "graphic:type": { $eq: "room" } })
+        .toArray();
+      for (let i = 0; i < _ROOM_PROP.length; i++) {
+        _ROOM_PROP[i]["_id"].toString();
+        let feature = {
+          type: "Feature",
+          properties: _ROOM_PROP[i],
+          geometry: {
+            type: "Polygon",
+            coordinates: [],
+          },
+        };
+        let faceroom = await database
+          .collection("FACE")
+          .findOne({ id_body: _ROOM_PROP[i]._id.toString() });
+        // tìm tọa độ
+        let geometries = await (
+          await database
+            .collection("NODE")
+            .find({ id_face: faceroom._id.toString() })
+            .sort(["index"], 1)
+            .toArray()
+        ).map((item) => item.geometry);
+
+        feature["geometry"]["coordinates"].push(geometries);
+        room["features"].push(feature);
+      }
+
+      resolve(room);
+    } catch (err) {
+      resolve({ error: err });
+      throw err;
     }
+  });
 }
-
-queryDataFloor();
-queryDataRoom();
 
 app.get("/", (req, res) => {
-    res.send("CHAO MUNG DEN VOI BLOCK B")
-})
+  res.send("CHAO MUNG DEN VOI BLOCK B");
+});
 app.get("/floor", cors(), (req, res) => {
-    res.send(floor)
-})
+  queryDataFloor(res).then(result => res.send(result))
+});
 app.get("/room", cors(), (req, res) => {
-    res.send(room)
-})
+  queryDataRoom(res).then(result => res.send(result));
+});
 app.listen(port, () => {
-    console.log("SERVER IS ON PORT 3000")
-})
-
-
-
-
-
-
-
+  console.log("SERVER IS ON PORT 3000");
+});
