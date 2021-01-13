fcd55a7f8823d7ef969367a1bc1006749d5128df
truonghuusang99 <truonghuusang99@gmail.com>
2021-01-13 07:16:55 +0700

    UPDATE

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const urlDB = "mongodb://localhost:27017/localhost";
+const urlDB = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 
-module.exports.mongo =new MongoClient(urlDB, { useUnifiedTopology: true });
+module.exports.mongo =new MongoClient(urlDB, { useUnifiedTopology: true});
     
